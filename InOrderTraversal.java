394fc29421dee9395cc5db7d40ea60528c8a8b9d
SuJingyu111 <gar123su@gmail.com>
2021-03-28 13:59:11 +0800

    94 in order Traversal  ---recursive and iterative ---medium

---
+++ InOrderTraversal.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+    }
+}
+
+public class InOrderTraversal {
+    class Solution {
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> result = new ArrayList<Integer>();
+            if (root == null) {
+                return result;
+            }
+            else if (root.left == null && root.right == null) {
+                result.add(root.val);
+                return result;
+            }
+            result.addAll(inorderTraversal(root.left));
+            result.add(root.val);
+            result.addAll(inorderTraversal(root.right));
+            return result;
+        }
+
+        public List<Integer> inorderTraversalIterative(TreeNode root) {
+            List<Integer> result = new ArrayList();
+            Deque<TreeNode> nodeStack = new LinkedList<TreeNode>();
+            while (root != null || !nodeStack.isEmpty()) {
+                while (root != null) {
+                    nodeStack.push(root);
+                    root = root.left;
+                }
+                root = nodeStack.pop();
+                result.add(root.val);
+                root = root.right;
+            }
+            return result;
+        }
+    }
+}
