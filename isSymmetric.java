567580d9bdf862d42a7ffa42df60a6ef293d7877
SuJingyu111 <gar123su@gmail.com>
2021-03-28 19:50:25 +0800

    101 Is Symmetric ---Recursive and Iterative Solution ---easy

--- isSymmetric.java
+++ isSymmetric.java
@@ -33,4 +33,52 @@ public class isSymmetric {
             return true;
         }
     }
+
+    class RecursiveSolution {
+        List<Integer> leftList = new ArrayList<Integer>();
+        List<Integer> rightList = new ArrayList<Integer>();
+        public boolean isSymmetric(TreeNode root) {
+            if (root.left == null && root.right == null)
+                return true;
+            if ( (root.left == null && root.right != null) || (root.left != null && root.right == null) )
+                return false;
+            leftDfs(root.left);
+            rightDfs(root.right);
+            if (rightList.size() != leftList.size())
+                return false;
+            for (int i = 0; i < leftList.size(); i++) {
+                if (leftList.get(i) != rightList.get(i))
+                    return false;
+            }
+            return true;
+        }
+
+        private void leftDfs(TreeNode root) {
+            leftList.add(root.val);
+            if (root.left != null) {
+                leftDfs(root.left);
+            }
+            else if (root.right != null) 
+                leftList.add(null);
+            if (root.right != null) {
+                leftDfs(root.right);
+            }
+            else if (root.left != null)
+                leftList.add(null);
+        }
+
+        private void rightDfs(TreeNode root) {
+            rightList.add(root.val);
+            if (root.right != null) {
+                rightDfs(root.right);
+            }
+            else if (root.left != null)
+                rightList.add(null);
+            if (root.left != null) {
+                rightDfs(root.left);
+            }
+            else if (root.right != null) 
+                rightList.add(null);
+        }
+    }
 }
