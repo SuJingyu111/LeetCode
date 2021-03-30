be810f8926c47465ed00b278edb8efd7cbf92b92
SuJingyu111 <gar123su@gmail.com>
2021-03-30 23:22:14 +0800

    190 Reverse Bits ---easy

---
+++ ReverseBits.java
@@ -0,0 +1,11 @@
+public class ReverseBits {
+    //This is the official answer
+    public int reverseBits(int n) {
+        int rev = 0;
+        for (int i = 0; i < 32 && n != 0; ++i) {
+            rev |= (n & 1) << (31 - i);
+            n >>>= 1;
+        }
+        return rev;
+    }
+}
