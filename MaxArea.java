ed0bc20b3cea2867f782664edf15554ac79c5bdd
SuJingyu111 <gar123su@gmail.com>
2021-03-31 01:10:32 +0800

    11 Max Area ---medium

--- MaxArea.java
+++ MaxArea.java
@@ -3,16 +3,15 @@ public class MaxArea {
         public int maxArea(int[] height) {
             int left = 0;
             int right = height.length - 1;
-            int result = Math.min(height[left], height[right]) * (right - left);
+            int result = 0;
             while (left < right) {
-                //System.out.println("----------------------");
-                if (height[left] < height[right]) {           
-                    result = Math.max(result, Math.min(height[left+1], height[right]) * (right - left - 1));
-                    left += 1;
+                if (height[left] < height[right]) { 
+                    result = result > height[left] * (right - left) ? result : height[left] * (right - left);       
+                    ++left;
                 }
-                else if (height[right] <= height[left]) {
-                    result = Math.max(result, Math.min(height[right - 1], height[left]) * (right - left - 1));
-                    right -= 1;
+                else {
+                    result = result > height[right] * (right - left) ? result : height[right] * (right - left); 
+                    --right;
                 }
             }
             return result;
