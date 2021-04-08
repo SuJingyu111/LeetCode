6c7dbf8084b37dc85c83165d58978b7e7a86f16e
SuJingyu111 <gar123su@gmail.com>
2021-04-08 19:11:37 +0800

    98 Is Valid BST ---Recurse & LDR solution ---medium

--- IsValidBST.java
+++ IsValidBST.java
@@ -10,4 +10,26 @@ public class IsValidBST {
             return root.val > lower && root.val < upper && recurse(root.left, lower, root.val) && recurse(root.right, root.val, upper);
         }
     }
+
+    class LDRSolution {
+        long last = Long.MIN_VALUE;
+    
+        public boolean isValidBST(TreeNode root) {     
+            return LDR(root);
+        }
+    
+        private boolean LDR(TreeNode root) {
+            if (root == null)
+                return true;
+            if (!LDR(root.left))
+                return false;
+            if (root.val > last) {
+                last = root.val;
+            }
+            else {
+                return false;
+            }
+            return LDR(root.right);
+        }
+    }
 }
