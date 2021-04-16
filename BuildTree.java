6137c81bb132a5596403e62b5f21846c0678126f
SuJingyu111 <gar123su@gmail.com>
2021-04-17 00:00:10 +0800

    102 Build Binary Tree ---medium

---
+++ BuildTree.java
@@ -0,0 +1,20 @@
+public class BuildTree {
+    class Solution {
+        public TreeNode buildTree(int[] preorder, int[] inorder) {
+            if (preorder.length == 0) 
+                return null;
+            if (preorder.length == 1)
+                return new TreeNode(preorder[0]);
+            TreeNode root = new TreeNode(preorder[0]);
+            int pos = 0;
+            while (inorder[pos] != root.val) {
+                pos++;
+            }
+            TreeNode left = buildTree(Arrays.copyOfRange(preorder, 1, 1 + pos), Arrays.copyOfRange(inorder, 0, pos));
+            TreeNode right = buildTree(Arrays.copyOfRange(preorder, 1 + pos, preorder.length), Arrays.copyOfRange(inorder, pos + 1, inorder.length));
+            root.left = left;
+            root.right = right;
+            return root;
+        }
+    }
+}
