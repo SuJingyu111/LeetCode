45875d0ef977fcb92dac82a6bb02f9a721526a3f
SuJingyu111 <gar123su@gmail.com>
2021-04-19 11:56:24 +0800

    142 Detect Cycle In LinkedList ---medium

---
+++ DetectCycle.java
@@ -0,0 +1,14 @@
+public class DetectCycle {
+    public class Solution {
+        public ListNode detectCycle(ListNode head) {
+            Set<ListNode> set = new HashSet<ListNode>();
+            while (head != null) {
+                if (!set.add(head)) {
+                    return head;
+                }
+                head = head.next;
+            }
+            return null;
+        }
+    }
+}
