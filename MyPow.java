dca4c2e36b9d991a4fe63387baeecf56f185893d
SuJingyu111 <gar123su@gmail.com>
2021-04-27 00:33:20 +0800

    50 My Pow ---medium

---
+++ MyPow.java
@@ -0,0 +1,16 @@
+public class MyPow {
+    class Solution {
+        public double myPow(double x, int n) {
+            if (n == 0)
+                return 1;
+            if (n == 1)
+                return x;
+            if (n == -1)
+                return 1 / x;
+            double half = myPow(x, n / 2);
+            double res = myPow(x, n % 2);
+            return half * half * res;
+        }
+    }
+    
+}
