399d51d30b827e0b23d49a374f181698e5db6cdc
SuJingyu111 <gar123su@gmail.com>
2021-05-25 18:36:15 +0800

    300 Longest Increasing Subsequence ---meidum

---
+++ LengthOfLTS.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class LengthOfLTS {
+    class Solution {
+        public int lengthOfLIS(int[] nums) {
+            int len = nums.length;
+            int[] dp = new int[len];
+            Arrays.fill(dp, 1);
+            int max = 1;
+            for (int i = 1; i < len; i++) {
+                for (int j = i - 1; j >= 0; j--) {
+                    if (nums[j] < nums[i]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                max = Math.max(dp[i], max);
+            }
+            return max;
+        }
+    }
+}
